a260740e9c67e54232388c2af317d4a34a04baf2
Cunqi Xiao <Cunqi@users.noreply.github.com>
2024-05-25 22:20:30 -0700

    [CXPopover] rename insets to safeareaInsets

--- Sources/CXPopover/Popover/CXPopoverHelper.swift
+++ Sources/CXPopover/Popover/CXPopoverHelper.swift
@@ -21,13 +21,13 @@ class CXPopoverHelper {
         let origin = CXPopoverHelper.makeOrigin(
             containerSize: containerSize,
             contentSize: contentSize,
-            insets: insets,
+            safeAreaInsets: insets,
             anchor: anchor)
         
         return CGRect(origin: origin, size: contentSize)
     }
     
-    static func makeOrigin(containerSize: CGSize, contentSize: CGSize, insets: UIEdgeInsets, anchor: CXPopoverBehavior.Anchor) -> CGPoint {
+    static func makeOrigin(containerSize: CGSize, contentSize: CGSize, safeAreaInsets: UIEdgeInsets, anchor: CXPopoverBehavior.Anchor) -> CGPoint {
         let width = containerSize.width
         let height = containerSize.height
         
@@ -36,21 +36,21 @@ class CXPopoverHelper {
         
         switch anchor {
         case .top:
-            return CGPoint(x: (width - contentWidth) / 2, y: insets.top)
+            return CGPoint(x: (width - contentWidth) / 2, y: safeAreaInsets.top)
         case .bottom:
-            return CGPoint(x: (width - contentWidth) / 2, y: height - insets.bottom - contentHeight)
+            return CGPoint(x: (width - contentWidth) / 2, y: height - safeAreaInsets.bottom - contentHeight)
         case .leading:
-            return CGPoint(x: insets.left, y: (height - contentHeight) / 2)
+            return CGPoint(x: safeAreaInsets.left, y: (height - contentHeight) / 2)
         case .trailing:
-            return CGPoint(x: width - insets.right - contentWidth, y: (height - contentHeight) / 2)
+            return CGPoint(x: width - safeAreaInsets.right - contentWidth, y: (height - contentHeight) / 2)
         case .topLeading:
-            return CGPoint(x: insets.left, y: insets.top)
+            return CGPoint(x: safeAreaInsets.left, y: safeAreaInsets.top)
         case .topTrailing:
-            return CGPoint(x: width - insets.right - contentWidth, y: insets.top)
+            return CGPoint(x: width - safeAreaInsets.right - contentWidth, y: safeAreaInsets.top)
         case .bottomLeading:
-            return CGPoint(x: insets.left, y: height - insets.bottom - contentHeight)
+            return CGPoint(x: safeAreaInsets.left, y: height - safeAreaInsets.bottom - contentHeight)
         case .bottomTrailing:
-            return CGPoint(x: width - insets.right - contentWidth, y: height - insets.bottom - contentHeight)
+            return CGPoint(x: width - safeAreaInsets.right - contentWidth, y: height - safeAreaInsets.bottom - contentHeight)
         case .center:
             return CGPoint(x: (width - contentWidth) / 2, y: (height - contentHeight) / 2)
         }
